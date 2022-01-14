5df16ab7367ed1b0ea38fa8092a2b78cff5a0681
skippharaoh <caniggiathompson@gmail.com>
2022-01-13 19:22:53 -0500

    Commit 2 - Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+// DEPENDENCIES //
 const express = require('express')
 
 const app = express()
 const PORT = 3000
 
+// CONFIG //
 app.set('view engine', 'ejs')
 
+// DATABASE MODELS //
+const pokemon = require('./models/pokemon')
+
+// INDEX ROUTE
 app.get('/', (req,res)=>{
     res.send('🌊 Server is on!')
+})
+app.get('/pokemon', (req,res)=>{
+    res.send(pokemon)
 })
 
 app.listen(PORT, ()=>{
